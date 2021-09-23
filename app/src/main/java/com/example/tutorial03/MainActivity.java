a98a62ff940264bf8740889e1bca8a287f5cd3c3
Brijesh Vora <85928806+Bvora084@users.noreply.github.com>
2021-09-23 15:18:11 +0530

    commited

--- app/src/main/java/com/example/tutorial03/MainActivity.java
+++ app/src/main/java/com/example/tutorial03/MainActivity.java
@@ -4,35 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText edtusername ,edtpassword;
+    EditText edtusername ;
+    EditText edtpassword;
     Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void function(View view) {
 
         edtusername =findViewById(R.id.edtusername);
         edtpassword =findViewById(R.id.edtpassword);
         btnlogin =findViewById(R.id.btnlogin);
 
+        String valusername =edtusername.getText().toString();
+        String valpassword =edtpassword.getText().toString();
+        Log.i("LoginScreen","In onClick");
 
 
-    }
-
-    public void fun1(View view) {
-        if(edtusername.equals("admin007@gmail.com") && edtpassword.equals("1234567")) {
-            Intent a1 = new Intent(this, MainActivity2.class);
-            startActivity(a1);
-            finish();
-        }else{
-            Toast.makeText(this, "Username or Password is wrong", Toast.LENGTH_SHORT).show();
+        if (!Patterns.EMAIL_ADDRESS.matcher(valusername).matches()){
+            Toast.makeText(MainActivity.this, "Enter proper email address", Toast.LENGTH_SHORT).show();
         }
+
+
+        if(valusername.equals("admin007@gmail.com") && valpassword.equals("1234567")){
+            Log.i("LoginScreen", "In onClick If");
+
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            intent.putExtra("userdata",valusername);
+            startActivity(intent);
+            finish();
+
+            Toast.makeText(MainActivity.this, "Login Successfully ", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Wrong Username or Password", Toast.LENGTH_SHORT).show();
+            Log.i("LoginScreen", "In onClick else");
+        }
+
     }
 }
